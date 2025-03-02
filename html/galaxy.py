1a797008855a305e18c34cfdb11022d1560d45af
Chet Bortz <chet@bortz.co>
2025-03-02 14:18:59 -0500

    [fix] only lookup data when galaxy is plausible

--- html/galaxy.py
+++ html/galaxy.py
@@ -161,8 +161,6 @@ def main():
 		galaxy = dbShared.dbInsertSafe(path[0])
 		conn = dbShared.ghConn()
 		galaxyAdminList = dbShared.getGalaxyAdminList(conn, currentUser)
-		availablePlanetList = getPlanetList(conn, galaxy, 1)
-		availableResourceTypeList = getElectiveResourceTypeList(conn, galaxy, 1)
 		if galaxy.isdigit():
 			# get the galaxy details for edit
 			galaxyCursor = conn.cursor()
@@ -176,6 +174,8 @@ def main():
 				galaxyWebsite = galaxyRow[3]
 			galaxyCursor.close()
 			galaxyPlanetList = getPlanetList(conn, galaxy, 0)
+			availablePlanetList = getPlanetList(conn, galaxy, 1)
+			availableResourceTypeList = getElectiveResourceTypeList(conn, galaxy, 1)
 			galaxyResourceTypeList = getElectiveResourceTypeList(conn, galaxy, 0)
 			galaxyAdmins = dbShared.getGalaxyAdmins(conn, galaxy)
 			conn.close()
